7fbd54c183307416f7b00d4c1101b67fe2f5020d
florian-pe <67597287+florian-pe@users.noreply.github.com>
2022-03-15 09:13:00 +0100

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -26,7 +26,7 @@ object Main extends MainRoutes {
 //   )
 
 
-  def classify_transporteurs() : Obj = {
+  def classify_transporteurs(livraison: Livraison) : Obj = {
       Obj(
         "compatibles" -> Arr(),
         "partiellement-compatibles" -> Arr(),
@@ -34,97 +34,40 @@ object Main extends MainRoutes {
       )
   }
 
-//   @post("/json")
-//   def jsonEndpointObj() : Obj = {
-//   }
-
-  @get("/")
-  def hello(): String = myfunc()
-
-
-
-  @post("/reverse")
-  def reverse(request: Request): String =
-    new String(request.readAllBytes()).reverse + "aaa "
-
-//   @post("/reverse")
-//   def reverse(request: Request): String =
-//     new String(request.readAllBytes()).reverse + "aaa "
-
-
-//   @post("/json")
-//   def json(request: Request): String = {
-//     val json = ujson.read(request.readAllBytes())
-//     val json = ujson.read(request.text())
-//     "got it"
-//   }
-
-
-
-  @cask.postJson("/json2")
-  def jsonEndpoint(value1: ujson.Value, value2: Seq[Int]) = {
-    "OK " + value1 + " " + value2
-  }
-
-//   case class MyLivraison(value1: String)
-
-//   @cask.postJson("/json")
-//   def jsonEndpointObj(id: String, coordRetrait: ujson.Obj ): String = {
-//       "got it"
-//   }
-
-//   case class Coord(latitude: Float) {
-//   case object Coord(latitude: Float) {
-//     def InputParser() : Unit = {
-//     }
-//   }
-
-  @cask.postJson("/json")
-  def jsonEndpointObj(
-    id: String,
-    coordRetrait: Obj,
-    coordLivraison: Obj,
-    creneau: Obj,
-    VolumeInM3: Value,
-    PacketWeightInKg: Value
-  ): Obj = {
-    classify_transporteurs()
-  }
-
-//   @postJson("/test")
-//   def jsonEndpointObj(myjson: MyType ): String = {
-//     "/test it works"
-//   }
-
-  @postJson("/test2")
-  def jsonEndpointObj(livraison: Livraison ): String = {
-    "/test2: it works"
+  @postJson("/json")
+  def jsonEndpointObj(livraison: Livraison) : Obj = {
+    classify_transporteurs(livraison)
   }
 
 
   initialize()
 
-//   println(Point(50.0, 30.0))
-
 }
 
 
-case class Coord(from: Float, to: Float)
+case class Coord(latitude: Float, longitude: Float)
 object Coord {
-
   import upickle.default._
-
   implicit val rw: ReadWriter[Coord] = macroRW
 }
 
-// case class Livraison(id: String, coordRetrait: String)
-case class Livraison(id: String, coordRetrait: Coord)
-object Livraison {
-
+case class CreneauHoraire(start: String, end: String)
+object CreneauHoraire {
   import upickle.default._
-
-  implicit val rw: ReadWriter[Livraison] = macroRW
+  implicit val rw: ReadWriter[CreneauHoraire] = macroRW
 }
 
+case class Livraison(
+  id: String,
+  coordRetrait: Coord,
+  coordLivraison: Coord,
+  creneau: CreneauHoraire,
+  VolumeInM3 : Float,
+  PacketWeightInKg: Float
+)
+object Livraison {
+  import upickle.default._
+  implicit val rw: ReadWriter[Livraison] = macroRW
+}
 
 
