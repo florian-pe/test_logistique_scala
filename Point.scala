93bc9d9595fa0217b583db59f24eb4595bcd7a8e
florian-pe <67597287+florian-pe@users.noreply.github.com>
2022-03-15 08:35:20 +0100

    Update Point.scala

--- Point.scala
+++ Point.scala
@@ -1,6 +1,14 @@
-package distances.model
+// package distances.model
+package distances
+
+package object model {
+
+type Latitude  = Double
+type Longitude = Double
 
 case class Point(latitude: Latitude, longitude: Longitude) {
 
   def toRadians: Point = Point(math.toRadians(latitude), math.toRadians(longitude))
+}
+
 }
